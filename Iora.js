41b653b78be67cc514246afd698f1f6af4cebc8d
Farhan <animationfar@gmail.com>
2022-10-21 12:59:10 +0700

    feat: add boilerplate.

--- Iora.js
+++ Iora.js
@@ -8,7 +8,7 @@ import { GLTFLoader } from 'GLTFLoader';
 export default async function Iora(opts){
     const {container} = opts;
     const renderer = new THREE.WebGLRenderer(opts);
-
+    
     const scene = new THREE.Scene();
     const camera = new THREE.PerspectiveCamera( 75, container.clientWidth/container.clientHeight, 0.1, 1000 );
     camera.position.z = 5;
---
+++ Scene.js
@@ -0,0 +1,5 @@
+/**
+ * This is a boilerplate for ThreeJS. This boilerplate
+ * could be use as ESM Web or NodeJS Build.
+ */
+
